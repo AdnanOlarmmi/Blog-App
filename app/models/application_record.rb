fcc8be95c7a8ed1c49af169a40f8136f14849958
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-19 23:24:26 +0100

    Add Proper tables and Models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,28 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
-
-class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id', class_name: 'Post'
-  has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
-  has_many :likes, foreign_key: 'author_id', class_name: 'Like'
-end
-
-class Post < ApplicationRecord
-  belongs_to :author, foreign_key: 'author_id', class_name: 'User
-  has_many :comments, foreign_key: 'post_id', class_name: 'Comment'
-  has_many :likes, foreign_key: 'post_id', class_name: 'Like
-end
-
-class Comment < ApplicationRecord
-  belongs_to :author, foreign_key: 'author_id', class_name: 'User'
-  belongs_to :post, foreign_key: 'post_id', class_name: 'Post'
-end
-
-class Like < ApplicationRecord
-  belongs_to :author, foreign_key: 'author_id', class_name: 'User
-  belongs_to :post, foreign_key: 'post_id', class_name: 'Post
-end
-
-
-
--- db/migrate/20230118195304_add_post_ref_to_comments.rb
+++ db/migrate/20230118195304_add_post_ref_to_comments.rb
@@ -1,5 +1,5 @@
 class AddPostRefToComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :post_id, null: false, foreign_key: {to_table: 'comments'}
+    add_reference :comments, :post, null: false, foreign_key: {to_table: 'posts'}
   end
 end
