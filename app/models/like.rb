ba44f19e1634dd808d5651ba4f940a8150e23d13
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-19 23:36:56 +0100

    Like Methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
     belongs_to :author, foreign_key: 'author_id', class_name: 'User'
     belongs_to :post, foreign_key: 'post_id', class_name: 'Post'
+
+    def update_like_counts
+        post = Post.find(self.post_id)
+        post.like_count = Like.where(post_id: post.id).count
+        post.save
+    end
 end
