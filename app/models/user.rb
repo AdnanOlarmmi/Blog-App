e5add9f6e1a884ee19df99b480a42950d8623400
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-19 23:28:48 +0100

    Method for first five posts in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :posts, foreign_key: 'author_id', class_name: 'Post'
     has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
     has_many :likes, foreign_key: 'author_id', class_name: 'Like'
+
+    def query_posts
+        posts = Post.where(author_id: self.id).order(created_at: :desc).limit(5)
+        return posts
+    end      
 end
