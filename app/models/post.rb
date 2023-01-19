496e7bf11e68999122641570bcd09bc240c26293
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-19 23:33:59 +0100

    Post methods done

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,16 @@ class Post < ApplicationRecord
     belongs_to :author, foreign_key: 'author_id', class_name: 'User'
     has_many :comments, foreign_key: 'post_id', class_name: 'Comment'
     has_many :likes, foreign_key: 'post_id', class_name: 'Like'
+
+    def query_comments
+        comments  = Comment.where(post_id: self.id).order(created_at: :desc).limit(5)
+        return comments
+    end
+
+    def update_post_counts
+        author = User.find(self.author_id)
+        author.post_count = Post.where(author_id: author.id).count
+        author.save
+    end
 end
 
