476f088c17c3a0b3170d45fb6fcb61dcb611c457
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-19 23:35:28 +0100

    Comment methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ApplicationRecord
     belongs_to :author, foreign_key: 'author_id', class_name: 'User'
     belongs_to :post, foreign_key: 'post_id', class_name: 'Post'
+
+    def update_comment_counts
+        post = Post.find(self.post_id)
+        post.comment_count = Comment.where(post_id: post.id).count
+        post.save
+    end
 end
 
