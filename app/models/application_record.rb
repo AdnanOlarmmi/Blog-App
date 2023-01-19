9f7662cf7ade6bffc0d7e0f25930efa37a679dad
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-19 19:47:51 +0100

    Change foreign key to post_id

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,17 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
+
+class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id', class_name: 'Post'
+  has_many :comments, foreign_key: 'author_id', class_name: 'Comment'
+  has_many :likes, foreign_key: 'author_id', class_name: 'Like'
+end
+
+class Post < ApplicationRecord
+  belongs_to :author, foreign_key: 'author_id', class_name: 'User
+  has_many :comments, foreign_key: 'post_id', class_name: 'Comment'
+  has_many :likes, foreign_key: 'post_id', class_name: 'Like
+end
+
+
