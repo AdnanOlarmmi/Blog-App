c656ddcf21472b5410d8a98661711f592837d21c
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-19 12:59:24 +0100

    Add author_id as foreign key

--- db/migrate/20230118195424_add_user_ref_likes.rb
+++ db/migrate/20230118195424_add_user_ref_likes.rb
@@ -1,5 +1,5 @@
 class AddUserRefLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :likes, :user, null: false, foreign_key: {to_table: 'users'}
+    add_reference :likes, :author_id, null: false, foreign_key: {to_table: 'users'}
   end
 end
