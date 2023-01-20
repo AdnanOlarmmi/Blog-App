c609240f52ca38a895d919b3c1b758c3ed394e50
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-20 15:29:46 +0100

    Complete Comment Test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
     context 'validations' do
         it 'is valid with valid attributes' do
-        expect(Comment.new(author_id: 1, post_id: 1, content: 'Lorem Ipsum')).to be_valid
+        
+        first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+        first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+        expect(Comment.new(post: first_post, author: first_user, text: 'Hi Tom!' )).to be_valid
         end
         it 'is not valid without a content' do
         expect(Comment.new).to_not be_valid
