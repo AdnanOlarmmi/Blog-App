840193d3743775f6e7a7b6ca420e3d40587c4a52
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-20 13:05:08 +0100

    Add User Tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id', class_name: 'Like'
 
   def query_posts
-    Post.where(author_id: id).order(created_at: :desc).limit(3)
+    Post.order(created_at: :desc).limit(3)
   end
 
   validates :name, presence: true
-  validates :postscounter, numericality { only_integer: true}, allow_nil: false
+  validates :postscounter, numericality: { only_integer: true }, allow_nil: true
 end
+
