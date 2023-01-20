283f914e5faa95d369267f294c5b478469949389
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-20 15:52:07 +0100

    Correct numericality in user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   end
 
   validates :name, presence: true
-  validates :postscounter, numericality: { only_integer: true }, allow_nil: true
+  validates :postscounter, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 end
