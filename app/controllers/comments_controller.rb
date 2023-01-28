0399c3353f6cf056285e5efd71bc2d4490aeb8d6
AdnanOlarmmi <olarmmi@gmail.com>
2023-01-28 10:54:47 +0100

    Fix Linters Error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
-   def create
-        @comment = Comment.new(
-        text: params[:text],
-        post_id: params[:post_id]
-        )
-        @comment.author_id = current_user.id
-        if @comment.save
-        flash[:notice] = 'Comment was successfully created.'
-        redirect_to user_post_path(params[:user_id], params[:post_id])
-        else
-        flash.now[:alert] = 'Comment was not created.'
-        render 'posts/show'
-        end
+  def create
+    @comment = Comment.new(
+      text: params[:text],
+      post_id: params[:post_id]
+    )
+    @comment.author_id = current_user.id
+    if @comment.save
+      flash[:notice] = 'Comment was successfully created.'
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    else
+      flash.now[:alert] = 'Comment was not created.'
+      render 'posts/show'
     end
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     respond_to do |format|
-      format.html{ render :new, locals: { post: @post } }
+      format.html { render :new, locals: { post: @post } }
     end
   end
 
@@ -30,5 +30,4 @@ class PostsController < ApplicationController
       render :new
     end
   end
-
 end
